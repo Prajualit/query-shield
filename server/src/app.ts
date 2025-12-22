f9002d284177d75ffa55e95b62db554632540a36
Prajualit Tickoo <23bec072@nith.ac.in>
2025-12-23 00:17:26 +0530

    feat: integrate routes and database into Express application

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes';
+import firewallRoutes from './routes/firewall.routes';
+import ruleRoutes from './routes/rule.routes';
 
 const app = express();
 
@@ -8,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/firewalls', firewallRoutes);
+app.use('/api/firewalls/:firewallId/rules', ruleRoutes);
 
 app.use((err: any, req: any, res: any, next: any) => {
   const statusCode = err.statusCode || 500;
